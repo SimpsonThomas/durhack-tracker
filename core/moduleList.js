fd2340b21b8488bfad38703c6737e8d4b61207b4
Thomas Simpson <46006748+SimpsonThomas@users.noreply.github.com>
2020-11-14 17:18:36 +0000

    Basic Module button overview

--- core/moduleList.js
+++ core/moduleList.js
@@ -1,7 +1,22 @@
+import styles from "../styles/course.module.css"
+
 function ModuleList() {
     return (
-            <div></div>
+            <div className={styles.moduleDiv}>
+                {moduleButton()}
+                {moduleButton()}
+            </div>
         )
+}
+
+function moduleButton() {
+    return (
+        <li className={styles.moduleButtonList}>
+            <button className={styles.moduleButton}>
+                Test
+            </button>
+        </li>
+    )
 }
 
 export default ModuleList
